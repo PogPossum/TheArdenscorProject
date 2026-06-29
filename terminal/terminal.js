7b05e21abc8ae3b32d694b9a95a91d65d7b6bf20
Ashley Ardenscor <ashleyardenscor@gmail.com>
2026-06-29 13:58:26 +0200

    Update terminal.js

--- terminal/terminal.js
+++ terminal/terminal.js
@@ -296,7 +296,7 @@ class DOSTerminal {
     this.addLine('[]    Mercantec, Viborg', 'system-text');
     this.addLine('[] -- Hovedforløb 2 - Data og Kommunikation / Mercantec, Viborg', 'system-text');
     this.addLine('[] -- Cloud CEDCE Kursus / ULC Erhvervsakademi og', 'system-text');
-    this.addLine('      Professionshøjskole, Oddense', 'system-text');
+    this.addLine('[]    Professionshøjskole, Oddense', 'system-text');
     this.addLine('[] -- Hovedforløb 1 - Data og Kommunikation / Mercantec, Viborg', 'system-text');
     this.addLine('[] -- Grundforløb 2 - Data og Kommunikation / Tradium, Randers', 'system-text');
     this.addLine('[] -- Medie og Journalistik / FGU, Aarhus ', 'system-text');
